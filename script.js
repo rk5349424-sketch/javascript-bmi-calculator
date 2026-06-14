fe48a0eae121ebfa5183fe0896967172e683a235
rk5349424-sketch <rk5349424@gmail.com>
2026-06-14 10:54:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+const height =  parseInt(document.querySelector('#height').value);
+const weight = parseInt(document.querySelector('#weight').value);
+const results = document.querySelector('#results');
+
+ if(height === ''  || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give  a valid height ${height}`;
+
+
+ }
+
+ if(height === ''  || height < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give  a valid height ${weight}`;
+ } else {
+   const bmi = (weight / ((height*height)/10000)).toFixed(2) 
+   //show the result
+   results.innerHTML = `<span>${bmi}</span>`;
+   let message = "";
+
+    if (bmi < 18.5) {
+        message = "Underweight";
+    } 
+    else if (bmi >= 18.5 && bmi <= 24.9) {
+        message = "Normal Range";
+    } 
+    else {
+        message = "Overweight";
+    }
+
+    results.innerHTML +=`<br><strong>${message}</strong>`;
+
+
+ }
+ 
+
+
+});
